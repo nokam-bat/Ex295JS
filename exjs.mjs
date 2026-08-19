1325cfd1eed98a40148d9312a8b3ecb13f6f4150
noork <noor.kammoun99@gmail.com>
2026-08-19 18:48:09 +0200

    Ex02 (boucle) du premier fichier d'exercices javascript qui servent à se remémorer js

--- exjs.mjs
+++ exjs.mjs
@@ -13,7 +13,7 @@ function showPlaylist(){
     for( let name of playlist) {
         console.log(name)
     }
-};
+}
 showPlaylist();
 
 //e
@@ -26,3 +26,10 @@ showPlaylist();
 
 //g
 playlist.splice(6,1);
+
+//Exo2
+let x = 1;
+while (x <= 9){
+    console.log(String(x).repeat(x))
+    x++
+}
